7db171ab7ca722027f8562124e135cd7fa38f045
Lena <finlandka@yandex.ru>
2023-03-09 10:47:36 +0300

    feat: load array

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,61 @@
-let page = document.querySelector('.page');
-let editButton = page.querySelector('.button_action_edit');
-let closeButton = page.querySelector('.button_action_close');
-let form = page.querySelector('.form');
-let popup = page.querySelector('.popup');
-let fullname = page.querySelector('.profile__fullname');
-let position = page.querySelector('.profile__position');
-let formName = page.querySelector('#name');
-let formPosition = page.querySelector('#position');
+const page = document.querySelector('.page');
+const editButton = page.querySelector('.button_action_edit');
+const closeButton = page.querySelector('.button_action_close');
+const form = page.querySelector('.form');
+const popup = page.querySelector('.popup');
+const fullname = page.querySelector('.profile__fullname');
+const position = page.querySelector('.profile__position');
+const formName = page.querySelector('#name');
+const formPosition = page.querySelector('#position');
+
+const initialCards = [
+  {
+    name: 'Байкал',
+    link: 'https://finlandka.github.io/mesto/images/gallery-baikal.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://finlandka.github.io/mesto/images/gallery-kamchatka.jpg'
+  },
+  {
+    name: 'Кижи',
+    link: 'https://finlandka.github.io/mesto/images/gallery-kiji.jpg'
+  },
+  {
+    name: 'Онежское озеро',
+    link: 'https://finlandka.github.io/mesto/images/gallery-lake-onejskoe.jpg'
+  },
+  {
+    name: 'Рыбачий полуостров',
+    link: 'https://finlandka.github.io/mesto/images/gallery-pov-rybaci.jpg'
+  },
+  {
+    name: 'Сочи',
+    link: 'https://finlandka.github.io/mesto/images/gallery-sochi.jpg'
+  }
+];
+
+function loadGallery() {
+  const gallerySection = page.querySelector('.gallery-section');
+  const galleryUl = document.createElement('ul');
+  galleryUl.classList.add('gallery');
+  gallerySection.append(galleryUl);
+
+  initialCards.forEach((image) => {
+    const galleryItem = document.createElement('li');
+    galleryItem.classList.add('gallery__item');
+    galleryItem.innerHTML = `
+                            <img class="gallery__pic" src="${image.link}" alt="${image.name}">
+                            <div class="gallery__desc">
+                              <h2 class="gallery__title">${image.name}</h2>
+                              <button class="button heart"></button>
+                            </div>
+    `;
+    galleryUl.append(galleryItem);
+  })
+}
+
+loadGallery();
 
 function openEditProfile() {
   popup.classList.add('popup_opened');
