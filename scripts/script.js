8ed50240352b48d395c55ec32f6a9a2f8aee08d5
finlandka <finlandka@yandex.ru>
2023-03-13 13:57:36 +0300

    fix: logic script

--- scripts/script.js
+++ scripts/script.js
@@ -10,12 +10,21 @@ const buttonEdit = page.querySelector('.button_action_edit');
 
 const popup = page.querySelector('.popup');
 const popupContainer = page.querySelector('.popup__container');
+
 const fullname = page.querySelector('.profile__fullname');
 const position = page.querySelector('.profile__position');
-const formName = page.querySelector('#name');
-const formPosition = page.querySelector('#position');
 
-const lightbox = page.querySelector('.lightbox');
+const popupProfileEdit = page.querySelector('.popup_edit-profile');
+const popupCardAdd = page.querySelector('.popup_add-card');
+const popupImage = page.querySelector('.popup_image');
+
+const slideImage = popupImage.querySelector('.popup__image');
+const slideImageDesc = page.querySelector('.popup__desc');
+
+const formInput = popupProfileEdit.querySelectorAll('.form__input');
+const buttonInput = popupProfileEdit.querySelector('.form__button');
+const formAdd = popupCardAdd.querySelectorAll('.form__input');
+const buttonInputAdd = popupCardAdd.querySelector('.form__button');
 
 //массив с картинками
 let initialCards = [
@@ -45,7 +54,7 @@ let initialCards = [
   }
 ];
 
-//функция загрузки картинок на страницу
+//функция загрузки картинок на страницу и действий с ними
 function loadGallery() {
   initialCards.forEach((image) => {
     const templateGalleryItem = templateGallery.content.cloneNode(true);
@@ -74,94 +83,83 @@ function loadGallery() {
     });
   }
 
-  //работа лайтбокса
+//слушаем картинки по клику
   const galleryPics = page.querySelectorAll('.gallery__pic');
-  const lightboxTitle = page.querySelector('.lightbox__title');
   for (let item of galleryPics) {
     item.addEventListener('click', () => {
-      lightbox.classList.toggle('lightbox_opened');
-      const lightboxImage = page.querySelector('.lightbox__image');
-      lightboxImage.src = item.src;
-      lightboxImage.alt = item.alt;
-      lightboxTitle.textContent = item.alt;
+      openPopup(popupImage);
+      loadImg(item);
+    });
+  }
+
+}
+
+//функция загрузки картинки и описания в попап
+function loadImg(img) {
+  slideImage.src = img.src;
+  slideImage.alt = img.alt;
+  slideImageDesc.textContent = img.alt;
+}
+
+  //функция отправки формы изменения данных профиля
+  function handleFormSubmit (form) {
+    fullname.textContent = form[0].value;
+    position.textContent = form[1].value;
+    closePopup(popupProfileEdit);
+  }
+
+  
+
+  //функция добавления картинок в массив
+  function uploadPicture (form) {
+    initialCards.push({name: form[0].value, link: form[1].value});
+    galleryUl.innerHTML = '';
+    loadGallery();
+    closePopup(popupCardAdd);
+  }
+
+
+//функцмя открытия попапа
+function openPopup(popup) {
+  popup.classList.toggle('popup_opened');
+  if (popup.classList.contains('popup_edit-profile')) {
+    formInput[0].value = fullname.textContent;
+    formInput[1].value = position.textContent;
+    buttonInput.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      handleFormSubmit(formInput);
     })
   }
 
-  //закрытие лайтбокса
-  const buttonCloseLigtbox = lightbox.querySelector('.button_action_close');
-  buttonCloseLigtbox.addEventListener('click', closeLightbox);
-
+  if (popup.classList.contains('popup_add-card')) {
+    buttonInputAdd.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      uploadPicture(formAdd);
+    });
+  }
 }
 
-//функция закрытия лайтбокса
-function closeLightbox() {
-  lightbox.classList.remove('lightbox_opened');
-}
-
-//функция открытия попапа и вставки содержимого в зависимости от кнопки
-function openPopup(popupElement, head, button) {
-  page.querySelector('.popup__title')?.remove();
-  page.querySelector('.form')?.remove();
-  popup.classList.toggle('popup_opened');
-  const template = page.querySelector('#templateForm').content;
-  const templateForm = template.cloneNode(true);
-  templateForm.querySelector('.popup__title').textContent = head;
-  templateForm.querySelector('.form__button').textContent = button;
-  popupContainer.append(templateForm);
-  const form = page.querySelector('.form');
-  const formInput = page.querySelectorAll('.form__input');
-
-  if (popupElement === 'edit') {
-    formInput[0].value = fullname.textContent;
-    formInput[0].setAttribute('id', 'name');
-    formInput[1].value = position.textContent;
-    formInput[1].setAttribute('id', 'position');
-
-    form.addEventListener('submit', handleFormSubmit);
-  }
-
-  if (popupElement === 'add') {
-    formInput[0].placeholder = 'Название';
-    formInput[0].setAttribute('id', 'namePlace');
-    formInput[1].placeholder = 'Ссылка на картинку';
-    formInput[1].setAttribute('id', 'linkImage');
-
-    form.addEventListener('submit', uploadPicture);
-  }
-
-  //функция отправки формы изменения данных профиля
-  function handleFormSubmit (evt) {
-    evt.preventDefault();
-    fullname.textContent = formInput[0].value;
-    position.textContent = formInput[1].value;
-    closePopup();
-  }
-
-  //функция добавления картинок в массив и на страницу
-  function uploadPicture (evt) {
-    evt.preventDefault();
-    initialCards.push({name: formInput[0].value, link: formInput[1].value});
-    galleryUl.innerHTML = '';
-    loadGallery();
-    closePopup();
-  }
-
-  //закрытие попапа
-  const buttonClosePopup = popup.querySelector('.button_action_close');
-  buttonClosePopup.addEventListener('click', closePopup);
-
-}
+//слушаем кнопки
+buttonEdit.addEventListener('click', () => openPopup(popupProfileEdit));
+buttonAdd.addEventListener('click', () => openPopup(popupCardAdd));
 
 //функция закрытия попапа
-function closePopup() {
+function closePopup(popup) {
   popup.classList.remove('popup_opened');
 }
+
+//слушание кнопок закрытия
+const buttonCloseEdit = popupProfileEdit.querySelector('.button_action_close');
+buttonCloseEdit.addEventListener('click', () => closePopup(popupProfileEdit));
+
+const buttonCloseAdd = popupCardAdd.querySelector('.button_action_close');
+buttonCloseAdd.addEventListener('click', () => closePopup(popupCardAdd));
+
+const buttonCloseImage = popupImage.querySelector('.button_action_close');
+buttonCloseImage.addEventListener('click', () => closePopup(popupImage));
 
 //загрузка картинок на страницу
 loadGallery();
 
-//кнопки добавления и редактирования
-buttonAdd.addEventListener('click', () => openPopup('add', 'Новое место', 'Создать'));
-buttonEdit.addEventListener('click', () => openPopup('edit', 'Редактировать профиль', 'Сохранить'));
 
 
